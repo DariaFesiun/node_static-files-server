43ef3989a0f8a285a0628999a0c27b5258439f3c
daradanilova <lemma1102@gmail.com>
2025-01-12 13:50:29 +0200

    Solution

--- src/createServer.js
+++ src/createServer.js
@@ -4,13 +4,13 @@ const fs = require('fs');
 
 function createServer() {
   const server = http.createServer(async (req, res) => {
-    const { pathname } = new URL(req.url || '', `http://${req.headers.host}`);
-    const requestedPath = pathname.replace('/file/', '') || 'index.html';
+    const { pathname } = new URL(req.url, `http://${req.headers.host}`);
+    const requestedPath = pathname.replace('/file', '') || 'index.html';
     const realPath = path.join(__dirname, '..', 'public', requestedPath);
 
     res.setHeader('Content-Type', 'text/plain');
 
-    if (!pathname.startsWith('/file/')) {
+    if (!pathname.startsWith('/file')) {
       res.statusCode = 400;
       res.end('Routes not starting with /file/');
 
